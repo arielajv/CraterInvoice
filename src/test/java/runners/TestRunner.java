f519050954f6e1dd6471095b879360cbf64f97c7
arielajv <valentina.dube@outlook.com>
2023-09-20 13:23:38 -0400

    Initial commit

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,21 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty",
+                "html:Cucumber_reports/cucumber_report.html",
+                "json:Cucumber_reports/cucumber_report.json"},
+        features="./src/test/resources/features",
+        glue="tests",
+        tags="@",
+        dryRun = false
+
+)
+
+public class TestRunner {
+
+}
